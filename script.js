c8ccd600d31c3300e1504011af4ff9228506ea22
Erhard Nagy <Erson1989x@gmail.com>
2024-05-17 18:40:29 +0300

    v2

--- script.js
+++ script.js
@@ -6,11 +6,34 @@ const dismissButton = document.querySelector('.dismiss-button');
 
 newsLetterForm.addEventListener('submit', (event) => {
     event.preventDefault();
+     
+        // Get the email input element and its value
+        const emailInput = document.querySelector('#email');
+        const emailValue = emailInput.value;
 
-    // Toggle the 'show-hidden' class on the success card and main container
-    successCard.style.display = 'block';
-    mainContainer.style.display = 'none';
-});
+        const errorMessage = document.querySelector('.error-message');
+    
+        // Validate the email
+        if (validateEmail(emailValue)) {
+            // If the email is valid, show the success card and hide the main container
+            successCard.style.display = 'block';
+            mainContainer.style.display = 'none';
+            if (errorMessage) {
+                errorMessage.style.display = 'none';
+            }
+            emailInput.classList.remove('invalid-input');
+
+        } else {
+            emailInput.classList.add('invalid-input'); 
+            // Optionally, show an error message element, if it exists
+            const errorMessage = document.querySelector('.error-message');
+            if (errorMessage) {
+    
+                errorMessage.style.display = 'flex';
+                errorMessage.textContent = 'Valid email required';
+            }
+        }
+    });
 
 dismissButton.addEventListener('click', () => {
     successCard.style.display = 'none';
@@ -30,5 +53,10 @@ const updateDisplayProperties =() => {
     }
 }
 updateDisplayProperties();
+
+const validateEmail = (email) => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
 
 window.addEventListener('resize', updateDisplayProperties);
